cdf43f9701e9fac89db35f9ae99ee5142915a84f
Karthik <karthikk1162@gmail.com>
2026-04-10 13:00:43 +0530

    Implemented Offset tracking to solve the problems of consumer restarting and all !

--- server/server.py
+++ server/server.py
@@ -1,14 +1,16 @@
 import socket
 import threading
-import uuid
 
 HOST = '0.0.0.0'
 PORT = 5000
 
-message_queue = []
-in_flight = {}
+##Persistent message log 
+message_log = []
 
-queue_lock = threading.Lock()
+##Track consumer offsets
+consumer_offsets = {}
+
+lock = threading.Lock()
 
 
 def handle_client(client_socket, address):
@@ -24,48 +26,32 @@ def handle_client(client_socket, address):
 
             message = data.decode('utf-8')
 
-            ##Consumer GET request
-            if message == "GET":
+            ##Consumer request: GET <consumer_id> <offset>
+            if message.startswith("GET"):
+                _, consumer_id, offset = message.split()
+                offset = int(offset)
 
-                queue_lock.acquire()
+                lock.acquire()
 
-                if message_queue:
-                    msg = message_queue.pop(0)
-                    msg_id = str(uuid.uuid4())
-
-                    in_flight[msg_id] = msg
-
-                    response = f"{msg_id}|{msg}"
+                if offset < len(message_log):
+                    msg = message_log[offset]
+                    response = f"{offset}|{msg}"
                     client_socket.send(response.encode('utf-8'))
 
-                    print(f"[SENT] {msg} (ID: {msg_id})")
-
+                    print(f"[SEND] To {consumer_id} → {msg} (offset {offset})")
                 else:
                     client_socket.send(b"EMPTY")
 
-                queue_lock.release()
-
-            ##ACK from consumer
-            elif message.startswith("ACK"):
-
-                _, msg_id = message.split("|")
-
-                queue_lock.acquire()
-
-                if msg_id in in_flight:
-                    print(f"[ACK RECEIVED] {in_flight[msg_id]}")
-                    del in_flight[msg_id]
-
-                queue_lock.release()
+                lock.release()
 
             else:
                 ##Producer message
-                queue_lock.acquire()
+                lock.acquire()
 
-                message_queue.append(message)
-                print(f"[STORED] {message}")
+                message_log.append(message)
+                print(f"[APPENDED] {message} (offset {len(message_log)-1})")
 
-                queue_lock.release()
+                lock.release()
 
         except Exception as e:
             print(f"[ERROR] {e}")
